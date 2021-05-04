9d9647f83a837d7e0e110d10380e0214ac8c2aea
Sam Gilchrist <samofgil@ku.edu>
2021-05-03 19:59:41 -0500

    Color Preview
    -added in a basic color preview page
    -needs to be implemented as a link still

---
+++ Final Project/ColorPreview/ex.js
@@ -0,0 +1,19 @@
+function myFunc() {
+  var text1 = document.getElementById("1R");
+  var text2 = document.getElementById("1G");
+  var text3 = document.getElementById("1B");
+  var code1="#" + text1.value.toString(2) + text2.value.toString(2) + text3.value.toString(2);
+
+
+    alert("Color Code: " + code1 + ".");
+    document.getElementsByTagName('h2')[0].style.backgroundColor = code1;
+    document.getElementsByTagName('h2')[0].style.color = code1;
+
+  document.querySelector("#1R").value = "";
+  document.querySelector("#1G").value = "";
+  document.querySelector("#1B").value = "";
+  document.querySelector("#2R").value = "";
+  document.querySelector("#2G").value = "";
+  document.querySelector("#2B").value = "";
+
+}
