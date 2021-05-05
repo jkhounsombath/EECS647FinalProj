12183a4a6196f899ae1116a992522827f3774ee3
N <illusionhump89@gmail.com>
2021-05-05 12:56:00 -0500

    Create user now loads to user portal but does not get added to mysql table

--- Final Project/createUser.php
+++ Final Project/createUser.php
@@ -2,7 +2,8 @@
 $userName = $_POST["uName"];
 $pwd = $_POST["pwd1"];
 $email = $_POST["uemail"];
-$mysqli = new mysqli("mysql.eecs.ku.edu", "jkhounsombath", "ieng9eiF", "jkhounsombath");
+$id = mt_rand(100000, 999999);
+$mysqli = new mysqli("sql102.epizy.com", "epiz_28547065", "J4zeP5TfSqnDVe", "epiz_28547065_revLEDS");
 if ($mysqli->connect_errno)
 {
   printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -13,9 +14,11 @@ $worked = true;
 if ($worked == true)
 {
   $query = "INSERT INTO USER 
-            (UPASSWORD, UNAME, EMAIL)
+            (ID, UNAME, UPASSWORD, EMAIL)
             VALUES
-            ('$pwd', '$userName', '$email')";
+            ('$id', '$userName', '$pwd', '$email')";
+
+  header("Location: userPortal.html");
 }
 $mysqli->close();
 ?>
--- Final Project/login.php
+++ Final Project/login.php
@@ -1,3 +1,52 @@
+<?php
+$userName = $_POST["username"];
+$pwd = $_POST["password"];
+$mysqli = new mysqli("sql102.epizy.com", "epiz_28547065", "J4zeP5TfSqnDVe", "epiz_28547065_revLEDS");
+if ($mysqli->connect_errno)
+{
+  printf("Connect failed: %s\n", $mysqli->connect_error);
+  exit();
+}
+
+if(isset(['username'])){
+    $username = $_POST['username'];
+    $pwd = $_POST['password'];
+    $sql = "select * from USER where UNAME = '".$uname."' AND UPASSWORD = '".$password"'
+    limit 1";
+
+    $result = mysql_query($sql);
+
+    if(mysql_num_rows($result) == 1){
+        header("Location: userPortal.html");
+    }else{
+        echo "Wrong";
+    }
+}
+
+
+
+
+
+// if ($worked == true)
+// {
+//   $query = "INSERT INTO USER 
+//             (UPASSWORD, UNAME, EMAIL)
+//             VALUES
+//             ('$pwd', '$userName', '$email')";
+            
+//   header("Location: userPortal.html");
+// }
+// $mysqli->close();
+
+
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 
 <head>
@@ -12,7 +61,7 @@
     <h1 align="center">Revolutionized LEDs</h1>
     <div class="loginMain">
        
-        <form action="login.php" method="post">
+        <form action="php/login.php" method="post">
 
             <div class = "loginFieldsContainer">
                 <label class = "loginText" for= "username"> USERNAME </label>
