383b4efd5c1e685cc8acd4569f5216e743e752e3
N <illusionhump89@gmail.com>
2021-05-05 17:54:53 -0500

    TEST push

--- Final Project/createUser.php
+++ Final Project/createUser.php
@@ -1,24 +1,37 @@
 <?php
+
 $userName = $_POST["uName"];
 $pwd = $_POST["pwd1"];
 $email = $_POST["uemail"];
-$id = mt_rand(100000, 999999);
-$mysqli = new mysqli("sql102.epizy.com", "epiz_28547065", "J4zeP5TfSqnDVe", "epiz_28547065_revLEDS");
+$mysqli = new mysqli("mysql.eecs.ku.edu", "jkhounsombath", "ieng9eiF", "jkhounsombath");
+
+echo $userName;
+
+
 if ($mysqli->connect_errno)
 {
   printf("Connect failed: %s\n", $mysqli->connect_error);
   exit();
-}
-$worked = true;
+}else{
 
-if ($worked == true)
-{
   $query = "INSERT INTO USER 
-            (ID, UNAME, UPASSWORD, EMAIL)
+            (UPASSWORD, UNAME, EMAIL)
             VALUES
-            ('$id', '$userName', '$pwd', '$email')";
+            ('$pwd', '$userName', '$email')";
 
-  header("Location: userPortal.html");
+  //header("Location: userPortal.html");
+  echo "TEST";
 }
+  
+
+if ($mysql->query($query) === TRUE) {
+  echo "New record created successfully";
+  header("Location: userPortal.html");
+} else {
+  echo "Error";
+}
+
 $mysqli->close();
 ?>
+
+
--- Final Project/login.php
+++ Final Project/login.php
@@ -1,7 +1,12 @@
 <?php
+
+
 $userName = $_POST["username"];
 $pwd = $_POST["password"];
 $mysqli = new mysqli("sql102.epizy.com", "epiz_28547065", "J4zeP5TfSqnDVe", "epiz_28547065_revLEDS");
+
+
+
 if ($mysqli->connect_errno)
 {
   printf("Connect failed: %s\n", $mysqli->connect_error);
@@ -39,7 +44,6 @@ if(isset(['username'])){
 // $mysqli->close();
 
 
-
 ?>
 
 
@@ -47,47 +51,5 @@ if(isset(['username'])){
 
 
 
-<!DOCTYPE html>
-
-<head>
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-  </head>
-  
-  <body>
-
-    <h1 align="center">Revolutionized LEDs</h1>
-    <div class="loginMain">
-       
-        <form action="php/login.php" method="post">
-
-            <div class = "loginFieldsContainer">
-                <label class = "loginText" for= "username"> USERNAME </label>
-                <input type= "text"  name = "username">
-                <br>
-                <label class = "loginText" for= "password"> PASSWORD </label>
-                <input type = "password"  name = "password">
-                <br>
-                <div class = "loginButtons">
-                    <button type= "submit"> Login </button>
-                </div>
-            </div>
-        </form>
-                <div class = "loginButtons">
-                    <button onclick="window.location.href='createUser.html'"> Register </button>
-                </div>
-                
-            
-        
-        
-    </div>
-
-       
-  </body>
-
-
-</html>
 
 
